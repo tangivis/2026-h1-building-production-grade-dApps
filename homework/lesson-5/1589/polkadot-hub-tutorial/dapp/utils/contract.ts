c824bc8ab1b2f90fa579b87990d4283d20fff496
Yufei <yff.wang@outlook.com>
2026-01-31 00:50:55 +0800

    Homework5 from 1589 Yufei

--- homework/lesson-5/1589/polkadot-hub-tutorial/dapp/utils/contract.ts
+++ homework/lesson-5/1589/polkadot-hub-tutorial/dapp/utils/contract.ts
@@ -2,7 +2,9 @@ import { getContract } from 'viem';
 import { publicClient, getWalletClient } from './viem';
 import StorageABI from '../abis/Storage.json';
 
-export const CONTRACT_ADDRESS = '0x5CC307268a1393AB9A764A20DACE848AB8275c46'; // TODO: change when the paseo asset hub RPC URL is available, and the contract is redeployed
+// 0x5CC307268a1393AB9A764A20DACE848AB8275c46
+// wyf: 0xc0F338917311cDb50E0b827636457d07F4391FeD
+export const CONTRACT_ADDRESS = '0x364EdBcccC7670f8845A9528032E0aE1b646D0F8'; // TODO: change when the paseo asset hub RPC URL is available, and the contract is redeployed
 export const CONTRACT_ABI = StorageABI.abi;
 
 // Create a function to get a contract instance for reading
@@ -23,3 +25,4 @@ export const getSignedContract = async () => {
     client: walletClient,
   });
 };
+
